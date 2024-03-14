7f0f719d4ddd9c95a198cf1fe91cfa4c9e863bfd
Scott Schulte <157662548+sschulte24110@users.noreply.github.com>
2024-03-14 18:13:32 -0500

    Add to style.css and script.js.
    Add functions to create gridContainer and finding a random color and mouseevent to change colors of the squares.
    Add minor css changes. More to come.

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-const gridContainer = document.querySelector('.container');
+const gridContainer = document.querySelector('#grid-container');
+const userInput = document.querySelector('.button');
+const ititialGrid = 16;
+
+function getRandomColor () {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color +- letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function gridGenerator(index) {
+    gridContainer.innerHTML = "";
+
+    for (let i = 0; i < index; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j < index; j++) {
+            const col = document.createElement('div');
+            col.classList.add('cell');
+            row.appendChild(col);
+        }
+        gridContainer.appendChild(row);
+    }
+
+    const hoverEffect = document.querySelectorAll('.cell');
+
+    hoverEffect.forEach((element) => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = getRandomColor();
+        });
+    });
+}
